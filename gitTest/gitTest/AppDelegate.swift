d0e9838e51a647f81719cd1fd022dda68824d42e
Serhiy Medvedyev <leadgit@gmail.com>
2015-10-14 19:44:53 +0300

    commit DEV 3

--- gitTest/gitTest/AppDelegate.swift
+++ gitTest/gitTest/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         print("This is the DEV change")
         print("And This is ANOTHER DEV change")
+        print("And This is THIRD DEV change!!!")
         // Override point for customization after application launch.
         return true
     }
